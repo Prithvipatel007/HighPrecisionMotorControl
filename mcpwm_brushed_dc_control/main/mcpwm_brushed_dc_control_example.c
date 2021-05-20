7f1a7361d89bc91dc25b87df2abc3de33f582142
Prithvipatel007 <prithvipatel007@gmail.com>
2021-05-20 11:46:35 +0200

    pwm gpio and duty cycle set

--- mcpwm_brushed_dc_control/main/mcpwm_brushed_dc_control_example.c
+++ mcpwm_brushed_dc_control/main/mcpwm_brushed_dc_control_example.c
@@ -73,8 +73,8 @@ static void mcpwm_example_brushed_motor_control(void *arg)
     printf("Configuring Initial Parameters of mcpwm...\n");
     mcpwm_config_t pwm_config;
     pwm_config.frequency = 1000;    //frequency = 500Hz,
-    pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
-    pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
+    pwm_config.cmpr_a = 50;    //duty cycle of PWMxA = 0
+    pwm_config.cmpr_b = 50;    //duty cycle of PWMxb = 0
     pwm_config.counter_mode = MCPWM_UP_COUNTER;
     pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
     mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);    //Configure PWM0A & PWM0B with above settings
